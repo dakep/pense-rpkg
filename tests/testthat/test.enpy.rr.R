f858431495893c4e462b643992562f8efbedd31f
David <d.kepplinger@stat.ubc.ca>
2016-05-14 13:21:05 -0700

    Fix enpy.rr test case

--- tests/testthat/test.enpy.rr.R
+++ tests/testthat/test.enpy.rr.R
@@ -12,15 +12,15 @@ test_that("enpy.rr", {
         ##
         ## No penalization at all
         ##
-        lambda1 <- 0
-        lambda2 <- 0
+        alpha <- 0
+        lambda <- 0
 
-        new <- penseinit::enpy(X, y, lambda1 = lambda1, lambda2 = lambda2,
+        new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
                                deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
                                prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
                                py.nit = 5, en.tol = 1e-8)
 
-        target <- enpy(X, y, lambda1 = lambda1, lambda2 = lambda2,
+        target <- enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
                        deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
                        prosac = 0.8, clean.method = "proportion", prop = 0.4,
                        py.nit = 5, en.tol = 1e-8)
@@ -30,94 +30,6 @@ test_that("enpy.rr", {
         })
 
         expect_lt(sum(abs(diffs)), 1e-8)
-
-
-        ##
-        ## Ridge regression penalty
-        ##
-        lambda <- 0.1
-
-        # alpha <- 0
-        # rr.rr.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        #
-        # rr.mn.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        #
-        # alpha <- 1
-        # lr.rr.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        # lr.mn.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        #
-        # alpha <- 0.5
-        # en.rr.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        # en.mn.old <- penseinit::enpy(X, y, lambda1 = alpha * lambda, lambda2 = 0.5 * (1 - alpha) * lambda,
-        #                           deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-        #                           prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-        #                           py.nit = 5, en.tol = 1e-8)
-        #
-        #
-        # save(rr.rr.old, rr.mn.old,
-        #      lr.rr.old, lr.mn.old,
-        #      en.rr.old, en.mn.old,
-        #      file = "~/Desktop/tmp.rda")
-
-        lambda <- 0.1
-
-        alpha <- 0
-        rr.rr.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-
-        rr.mn.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-
-        alpha <- 1
-        lr.rr.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-        lr.mn.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-
-        alpha <- 0.5
-        en.rr.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "rr",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-        en.mn.new <- penseinit::enpy(X, y, alpha = alpha, lambda = lambda,
-                                     deltaesc = 0.5, cc.scale = 1.54764, psc.method = "Mn",
-                                     prosac = 0.8, clean.method = "proportion", prop = 0.4, C.res = NULL,
-                                     py.nit = 5, en.tol = 1e-8)
-
-
-        load("~/Desktop/tmp.rda")
-
-
-        sum(abs(rr.rr.new$coeff - rr.rr.old$coeff))
-        sum(abs(rr.mn.new$coeff - rr.mn.old$coeff))
-
-        sum(abs(lr.rr.new$coeff - lr.rr.old$coeff))
-        sum(abs(lr.mn.new$coeff - lr.mn.old$coeff))
-
-        sum(abs(en.rr.new$coeff - en.rr.old$coeff))
-        sum(abs(en.mn.new$coeff - en.mn.old$coeff))
     }
+
 })

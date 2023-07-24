430d9d44be44f8cc35efe8a0a7f053c89e6355a9
davidkep <david@rotinaj.org>
2023-07-24 09:31:48 -0400

    fix bug

--- R/elnet.R
+++ R/elnet.R
@@ -226,7 +226,7 @@ elnet_cv <- function (x, y, lambda, cv_k, cv_repl = 1,
           intercept = handler_args$args$intercept,
           optional_args = handler_args$args$optional_args)
 
-        lapply(cv_fit$estimates, `[[`, 1L)
+        cv_fit$estimates
       }
 
       set.seed(fit_seed)
